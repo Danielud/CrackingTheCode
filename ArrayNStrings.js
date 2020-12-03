d95ccd5ea01f9bd1b515165ff1e061570e89db1f
Daniel <danielud@gmail.com>
2020-12-03 14:05:24 +0200

    find unique character with data structure

--- ArrayNStrings.js
+++ ArrayNStrings.js
@@ -1,1 +1,17 @@
-// new change code -
+//TODO: 1.1 Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+
+function isUnique(str) {
+  console.log(str);
+  let oger = {};
+  for (var i = 0; i < str.length; i++) {
+    console.log(oger);
+    if (oger[str[i]]) {
+      return false;
+    } else {
+      oger[str[i]] = str[i];
+    }
+  }
+  return true;
+}
+
+console.log(isUnique("Daniel"));
